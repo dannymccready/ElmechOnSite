75e4c8a1e249fae40b4b86f1e9f125e62076ba2f
Keelin <keelin21@live.ie>
2018-07-16 14:46:42 +0100

    Fixed up startViewController, added autolayout to launch screen and svc

--- ElmechOnSite/ViewControllers/StartViewController.swift
+++ ElmechOnSite/ViewControllers/StartViewController.swift
@@ -8,3 +8,46 @@
 
 import Foundation
 import UIKit
+
+class StartViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onSiteSelfie(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "onSiteSelfie", sender: nil)
+    }
+    
+    @IBAction func elmechWebsite(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "elmechHomePage", sender: nil)
+    }
+    
+    @IBAction func newEmployee(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "newEmployee", sender: nil)
+    }
+    
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? UITabBarController {
+            if segue.identifier == "onSiteSelfie" {
+                destination.selectedIndex = 0
+            }
+            else if segue.identifier == "elmechHomePage" {
+                destination.selectedIndex = 1
+            }
+            else if segue.identifier == "newEmployee" {
+                destination.selectedIndex = 2
+            }
+        }
+     }
+    
+}
